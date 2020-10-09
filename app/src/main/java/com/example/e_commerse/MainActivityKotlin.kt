b2d3eae80fcc611b34a18c0016bcfea8f9525f87
mohitpanwar09 <sandypanwar09@gmail.com>
2020-10-09 17:08:22 +0530

    Kt_MainActivityDone

--- app/src/main/java/com/example/e_commerse/MainActivityKotlin.kt
+++ app/src/main/java/com/example/e_commerse/MainActivityKotlin.kt
@@ -1,22 +1,145 @@
 package com.example.e_commerse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.example.e_commerse.KotlinItemClass.CategoryItem
+import com.example.e_commerse.KotlinItemClass.MainItem
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import kotlinx.android.synthetic.main.activity_main_kotlin.*
+import java.util.*
 
-class MainActivityKotlin : AppCompatActivity() {
+
+class MainActivityKotlin : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_kotlin)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val drawerToggle = ActionBarDrawerToggle(this, drawer,R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        navigation_view.setNavigationItemSelectedListener(this)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val adapter=GroupAdapter<GroupieViewHolder>()
 
         adapter.add(CategoryItem())
         adapter.add(CategoryItem())
         adapter.add(CategoryItem())
+        adapter.add(CategoryItem())
+        adapter.add(CategoryItem())
+        adapter.add(CategoryItem())
+        adapter.add(CategoryItem())
+        adapter.add(CategoryItem())
+        adapter.add(CategoryItem())
+        adapter.add(CategoryItem())
+        adapter.add(CategoryItem())
+        adapter.add(CategoryItem())
         recycleView_category.adapter=adapter
+
+        val adapter2=GroupAdapter<GroupieViewHolder>()
+            adapter2.add(MainItem())
+            adapter2.add(MainItem())
+            adapter2.add(MainItem())
+            adapter2.add(MainItem())
+        recycleView_main_item.adapter=adapter2
+
+        val adapter3=GroupAdapter<GroupieViewHolder>()
+        adapter3.add(MainItem())
+        adapter3.add(MainItem())
+        adapter3.add(MainItem())
+        adapter3.add(MainItem())
+
+        recycleView_main_item_second.adapter=adapter3
+        fetchCategoryItem()
     }
+
+    private fun fetchCategoryItem(){
+       val ref= FirebaseDatabase.getInstance().getReference("/Categories")
+        ref.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                p0.children.forEach{
+                    Log.d("Get items", it.toString())
+                }
+            }
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_home -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+
+                val intent=Intent(this,MainActivityKotlin::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_orders -> {
+                Toast.makeText(this, "Orders", Toast.LENGTH_SHORT).show()
+
+                val intent=Intent(this,Orders::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_products -> {
+                Toast.makeText(this, "Products", Toast.LENGTH_SHORT).show()
+
+                val intent=Intent(this,Products::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_basket -> {
+                Toast.makeText(this, "Basket", Toast.LENGTH_SHORT).show()
+
+                val intent=Intent(this,Basket::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_customerCare -> {
+                Toast.makeText(this, "Customer care", Toast.LENGTH_SHORT).show()
+
+            }
+            R.id.nav_logout -> {
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+
+
+            }
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
