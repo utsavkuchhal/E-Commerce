9f8d331cc7a68cff2d0e161bc6b42f4e7e8d0406
mohitpanwar09 <sandypanwar09@gmail.com>
2020-10-15 22:20:33 +0530

    addCart functionality done

--- app/src/main/java/com/example/e_commerse/ktActivitys/UserProductActivity.kt
+++ app/src/main/java/com/example/e_commerse/ktActivitys/UserProductActivity.kt
@@ -2,15 +2,22 @@ package com.example.e_commerse.ktActivitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.widget.Toast
+import com.example.e_commerse.KotlinItemClass.CartInfo
 import com.example.e_commerse.KotlinItemClass.FetchItem
 import com.example.e_commerse.KotlinItemClass.UserReview
 import com.example.e_commerse.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import kotlinx.android.synthetic.main.activity_user_product.*
+import java.util.*
 
 class UserProductActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_product)
@@ -30,5 +37,20 @@ class UserProductActivity : AppCompatActivity() {
             adapter.add(UserReview())
             adapter.add(UserReview())
         user_product_review_recycleView.adapter=adapter
+
+        user_product_addCart.setOnClickListener {
+            addToCart()
+        }
+    }
+
+    private fun addToCart(){
+        val productDet=intent.getParcelableExtra<FetchItem>("ProductD")
+        val userUid=FirebaseAuth.getInstance().uid.toString()
+        val cartInfo=CartInfo("${productDet!!.productId}",1)
+
+        FirebaseDatabase.getInstance().getReference("basket").child(userUid).child(productDet!!.productId).setValue(cartInfo)
+                .addOnCompleteListener {
+                    Toast.makeText(this,"Added to cart",Toast.LENGTH_SHORT).show()
+                }
     }
 }
