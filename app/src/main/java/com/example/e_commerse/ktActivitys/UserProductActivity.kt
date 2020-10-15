1810f45fda4c10890ecc102b58b21fa682eeb059
mohitpanwar09 <sandypanwar09@gmail.com>
2020-10-15 17:52:56 +0530

    User product Display uiDone

--- app/src/main/java/com/example/e_commerse/ktActivitys/UserProductActivity.kt
+++ app/src/main/java/com/example/e_commerse/ktActivitys/UserProductActivity.kt
@@ -3,8 +3,11 @@ package com.example.e_commerse.ktActivitys
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.e_commerse.KotlinItemClass.FetchItem
+import com.example.e_commerse.KotlinItemClass.UserReview
 import com.example.e_commerse.R
 import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import kotlinx.android.synthetic.main.activity_user_product.*
 
 class UserProductActivity : AppCompatActivity() {
@@ -19,5 +22,13 @@ class UserProductActivity : AppCompatActivity() {
             user_product_price.text=productDetails.price.toString()
             user_product_des.text=productDetails.description
         }
+
+
+        val adapter=GroupAdapter<GroupieViewHolder>()
+            adapter.add(UserReview())
+            adapter.add(UserReview())
+            adapter.add(UserReview())
+            adapter.add(UserReview())
+        user_product_review_recycleView.adapter=adapter
     }
 }
