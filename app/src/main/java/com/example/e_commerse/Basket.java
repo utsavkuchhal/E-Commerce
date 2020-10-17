26c66348a7565f3cf79e8277bcaa95f577dd0009
mohitpanwar09 <sandypanwar09@gmail.com>
2020-10-18 04:04:34 +0530

    errorJava

--- app/src/main/java/com/example/e_commerse/Basket.java
+++ app/src/main/java/com/example/e_commerse/Basket.java
@@ -67,7 +67,7 @@ public class Basket extends AppCompatActivity implements BasketAdapter.ClickList
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     String productid = (String) snapshot.getKey();
-                    int quantity = Integer.parseInt(snapshot.getValue().toString());
+                    int quantity = Integer.parseInt(snapshot.getValue());
                     basketItems.add(new BasketModel(productid, quantity));
                 }
 
